25ac2d9b185dc549f6cf09397ff66a5e46c50c1c
jsnjuan <42633521+jsnjuan@users.noreply.github.com>
2026-05-10 01:23:45 -0600

    fix chronosport and metamx drivers

--- sites/chronosport_driver.py
+++ sites/chronosport_driver.py
@@ -1,43 +1,89 @@
 import asyncio
+import urllib.parse
 from sites.base_driver import BaseDriver, NO_MORE_PAGES
 
 class ChronosportDriver(BaseDriver):
 
     async def fetch_page(self, page, event_config, page_number):
-        # The API used by chronosport.mx / meta.mx (sportmaniacs) returns all the data in a single JSON 
-        # requested by the client, so we only need to fetch the data on page 1, and return None for 
-        # consecutive pages to stop, this in order to align the most complex pagination logic implemented 
-        # for other driver
+        # The API used by chronosport.mx / meta.mx (sportmaniacs) can be from 
+        # two types, it can return all data in the first request, or it can 
+        # take a json name from the 1st call and then call it to get the data JSON. 
+        # The type depends in the URL type        
         if page_number > 1:
             return NO_MORE_PAGES
 
-        def extract_competition_id(url):
-            try:
-                return url.split("#")[-1]
-            except Exception as e:
-                print("Error extracting competition_id: {e}")
-
+        def extract_competition_id(url, url_type):
+            if url_type==1:
+                try:
+                    return url.split("#")[-2].split('/')[-2]
+                except Exception as e:
+                    print(f"Error extracting competition_id: {e}")
+            elif url_type==2:
+                try:
+                    return url.split("/")[-2]
+                except Exception as e:
+                    print(f"Error extracting competition_id: {e}")
+            else:
+                return None
+        
         url = event_config['category_url']
-        catches = {"final_data":None}
+        data_json_urls = set()
+        url_type = None
+        if url.endswith("results#rankings"):
+            url_type = 1
+        elif url.endswith("/rankings"):
+            url_type = 2
+        else:
+            raise NotImplementedError("The URL type is not yet supported")
+                
+        
+        catches = {"metadata":None, "final_data":None}
 
-        competition_id = extract_competition_id(url)
+        competition_id = extract_competition_id(url, url_type)
 
         response_event = asyncio.Event()
 
         async def handle_response(response):
-            if competition_id in response.url:
-                content_type = response.headers.get("content-type", "")
-                if "application/json" not in content_type:
-                    return  # Skip non-JSON responses
-            
-                print("--- Data JSON detected in the network ---")
-                try:
-                    data = await response.json()
-                    catches["final_data"] = data
-                    response_event.set()
-                except Exception as e:
-                    print(f"Error processing JSON: {e}")
+            nonlocal url_type
+            nonlocal data_json_urls
+            if url_type == 1:
+                if competition_id in response.url:
+                    content_type = response.headers.get("content-type", "")
+                    if "application/json" not in content_type:
+                        return  # Skip non-JSON responses
+                
+                    try:
+                        data = await response.json()
+                        if isinstance(data.get("data"), dict) and "Rankings" in data.get("data", {}):
+                            print("--- Data JSON detected in the network ---")
+                            catches["final_data"] = data
+                            response_event.set()
+                    except Exception as e:
+                        print(f"Error processing JSON: {e}")
+            elif url_type == 2:
+                unquoted_url = urllib.parse.unquote(response.url)
+                if f"/rankings/{event_config['distance']}" in unquoted_url and not catches["metadata"]:
+                    print("--- First JSON detected in the network ---")
+                    try:
+                        metadata = await response.json()
+                        catches["metadata"] = metadata
 
+                        for item in metadata.get("data", []):
+                            if item.get("split") == "META" and item.get("type") == "":
+                                uri = item.get("resources", {}).get("uri")
+                                if uri:
+                                    data_json_urls.add(uri)
+
+                    except Exception as e:
+                        print(f"Error processing 1st JSON: {e}")
+                elif data_json_urls and any(candidate in unquoted_url for candidate in data_json_urls):
+                    print("--- Second JSON detected in the network ---")
+                    try:
+                        catches["final_data"] = await response.json()
+                        response_event.set()
+                    except Exception as e:
+                        print(f"Error processing 2nd JSON: {e}")
+                    pass
             
         page.on("response", handle_response)
         # We have processed up to this point
@@ -52,15 +98,28 @@ class ChronosportDriver(BaseDriver):
             
             page.remove_listener("response", handle_response)
             
-            data = catches["final_data"]["data"]["Rankings"]
-            if not data:
-                print("JSON data is None")
-                return None
-                
-            return {
-                "totalpages": 1,
-                "records": data
-            }
+            if url_type==1:
+                data = catches["final_data"]["data"]["Rankings"]
+                if not data:
+                    print("JSON data is None")
+                    return None
+                    
+                return {
+                    "totalpages": 1,
+                    "records": data
+                }
+            elif url_type==2:
+                data = catches["final_data"]
+                if not data:
+                    print("JSON data is None")
+                    return None
+
+                print("JSON data has keys:", data.keys())
+
+                return {
+                    "totalpages": 1,
+                    "records": data["data"]
+                }
 
         except Exception as e:
             print("Exception during page fetch:", e)
--- sites/metamx_driver.py
+++ sites/metamx_driver.py
@@ -5,50 +5,84 @@ from sites.base_driver import BaseDriver, NO_MORE_PAGES
 class MetamxDriver(BaseDriver):
 
     async def fetch_page(self, page, event_config, page_number):
-        # The API used by meta.mx/sportmaniacs returns all the data in a single JSON requested by the client
-        # So we only need to fetch the data on page 1, and return None for consecutive pages to stop, 
-        # this in order to align the most complex pagination logic implemented for other driver
+        # The API used by chronosport.mx / meta.mx (sportmaniacs) can be from 
+        # two types, it can return all data in the first request, or it can 
+        # take a json name from the 1st call and then call it to get the data JSON. 
+        # The type depends in the URL type
         if page_number > 1:
             return NO_MORE_PAGES
 
-        def extract_competition_id(url):
-            try:
-                return url.split("/")[-2]
-            except Exception as e:
-                print("Error extracting competition_id: {e}")
-
+        def extract_competition_id(url, url_type):
+            if url_type==1:
+                try:
+                    return url.split("#")[-2].split('/')[-2]
+                except Exception as e:
+                    print(f"Error extracting competition_id: {e}")
+            elif url_type==2:
+                try:
+                    return url.split("/")[-2]
+                except Exception as e:
+                    print(f"Error extracting competition_id: {e}")
+            else:
+                return None
+        
         url = event_config['category_url']
-        catches = {"metadata":None, "final_data":None}
         data_json_urls = set()
-        competition_id = extract_competition_id(url)
+        url_type = None
+        if url.endswith("results#rankings"):
+            url_type = 1
+        elif url.endswith("/rankings"):
+            url_type = 2
+        else:
+            raise NotImplementedError("The URL type is not yet supported")
+                
+        
+        catches = {"metadata":None, "final_data":None}
+
+        competition_id = extract_competition_id(url, url_type)
 
         response_event = asyncio.Event()
 
         async def handle_response(response):
+            nonlocal url_type
             nonlocal data_json_urls
-            unquoted_url = urllib.parse.unquote(response.url)
-            if f"/rankings/{event_config['distance']}" in unquoted_url and not catches["metadata"]:
-                print("--- First JSON detected in the network ---")
-                try:
-                    metadata = await response.json()
-                    catches["metadata"] = metadata
+            if url_type == 1:
+                if competition_id in response.url:
+                    content_type = response.headers.get("content-type", "")
+                    if "application/json" not in content_type:
+                        return  # Skip non-JSON responses
+                
+                    try:
+                        data = await response.json()
+                        if isinstance(data.get("data"), dict) and "Rankings" in data.get("data", {}):
+                            print("--- Data JSON detected in the network ---")
+                            catches["final_data"] = data
+                            response_event.set()
+                    except Exception as e:
+                        print(f"Error processing JSON: {e}")
+            elif url_type == 2:
+                unquoted_url = urllib.parse.unquote(response.url)
+                if f"/rankings/{event_config['distance']}" in unquoted_url and not catches["metadata"]:
+                    print("--- First JSON detected in the network ---")
+                    try:
+                        metadata = await response.json()
+                        catches["metadata"] = metadata
 
-                    for item in metadata.get("data", []):
-                        if item.get("split") == "META" and item.get("type") == "":
-                            uri = item.get("resources", {}).get("uri")
-                            if uri:
-                                data_json_urls.add(uri)
+                        for item in metadata.get("data", []):
+                            if item.get("split") == "META" and item.get("type") == "":
+                                uri = item.get("resources", {}).get("uri")
+                                if uri:
+                                    data_json_urls.add(uri)
 
-
-                except Exception as e:
-                    print(f"Error processing 1st JSON: {e}")
-            elif data_json_urls and any(candidate in unquoted_url for candidate in data_json_urls):
-                print("--- Second JSON detected in the network ---")
-                try:
-                    catches["final_data"] = await response.json()
-                    response_event.set()
-                except Exception as e:
-                    print(f"Error processing 2nd JSON: {e}")
+                    except Exception as e:
+                        print(f"Error processing 1st JSON: {e}")
+                elif data_json_urls and any(candidate in unquoted_url for candidate in data_json_urls):
+                    print("--- Second JSON detected in the network ---")
+                    try:
+                        catches["final_data"] = await response.json()
+                        response_event.set()
+                    except Exception as e:
+                        print(f"Error processing 2nd JSON: {e}")
                     pass
             
         page.on("response", handle_response)
@@ -58,26 +92,34 @@ class MetamxDriver(BaseDriver):
             await page.goto(url, wait_until="domcontentloaded")
             try:
                 await asyncio.wait_for(response_event.wait(), timeout=10.0)
-                print("Event set successfully, both JSONs captured")
+                print("Event set successfully, data JSON captured")
             except asyncio.TimeoutError:
-                if not catches["metadata"]:
-                    print("ERROR: 1st JSON was never captured | URL didn't match.")
-                elif not catches["final_data"]:
-                    print("ERROR: 1st JSON captured, 2nd JSON was never loaded.")
+                    print("ERROR: JSON was never captured")
             
             page.remove_listener("response", handle_response)
             
-            data = catches["final_data"]
-            if not data:
-                print("JSON data is None")
-                return None
+            if url_type==1:
+                data = catches["final_data"]["data"]["Rankings"]
+                if not data:
+                    print("JSON data is None")
+                    return None
+                    
+                return {
+                    "totalpages": 1,
+                    "records": data
+                }
+            elif url_type==2:
+                data = catches["final_data"]
+                if not data:
+                    print("JSON data is None")
+                    return None
 
-            print("JSON data has keys:", data.keys())
+                print("JSON data has keys:", data.keys())
 
-            return {
-                "totalpages": 1,
-                "records": data["data"]
-            }
+                return {
+                    "totalpages": 1,
+                    "records": data["data"]
+                }
 
         except Exception as e:
             print("Exception during page fetch:", e)
